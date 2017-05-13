55634c2a634bf1d8b6887a86a1fefaff6132b7fd
recallwolf <826628485@qq.com>
2017-05-13 11:38:28 +0800

    add auto reply

--- info_box/wechat/views.py
+++ info_box/wechat/views.py
@@ -1,19 +1,58 @@
 #coding=utf-8
 
 # Create your views here.
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render 
+from django.http.response import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
-import hashlib
+from wechat_sdk import WechatConf
+from wechat_sdk import WechatBasic
+from wechat_sdk.exceptions import ParseError
+from wechat_sdk.messages import (TextMessage, VoiceMessage, ImageMessage, VideoMessage, LinkMessage, LocationMessage, EventMessage, ShortVideoMessage)
+
+conf = WechatConf(
+    token='wechatinfobox', 
+    appid='wxfe5f1438b661e667', 
+    appsecret='81eca465fa6fb0ad8968e418712fe25d', 
+    encrypt_mode='normal',  # 可选项：normal/compatible/safe，分别对应于 明文/兼容/安全 模式
+    encoding_aes_key='yzJY6Q5IhOlJmd9A28qS4Q1chANB9rkLTqirRTNbTTG'  # 如果传入此值则必须保证同时传入 token, appid
+)
 
 @csrf_exempt
 def wechat(request):
-    token = 'wechatinfobox'    # 濉叆浣犵殑 Token
     signature = request.GET.get('signature', '')
     timestamp = request.GET.get('timestamp', '')
     nonce = request.GET.get('nonce', '')
-    # 璁＄畻鎺掑簭鍚庣殑鍝堝笇鍊煎苟姣旇緝
-    tmp_sig = hashlib.sha1(''.join(sorted([token, timestamp, nonce]))).hexdigest()
-    if tmp_sig == signature:
-        return HttpResponse(request.GET.get('echostr', ''))
-    return HttpResponse('error')
+	
+    wechat_instance = WechatBasic(conf=conf)
+    # 验证微信公众平台的消息
+    if wechat_instance.check_signature(signature!=signature,timestamp!=timestamp,nonce!=nonce):
+        return HttpResponseBadRequest('Verify Failed')
+    else:
+        if request.method == 'GET':
+           response = request.GET.get('echostr', 'error')
+        else:
+            try:
+                wechat_instance.parse_data(request.body)
+                message = wechat_instance.get_message()
+                # 判断消息类型
+                if isinstance(message, TextMessage):
+                    reply_text = 'text'
+                elif isinstance(message, VoiceMessage):
+                    reply_text = 'voice'
+                elif isinstance(message, ImageMessage):
+                    reply_text = 'image'
+                elif isinstance(message, LinkMessage):
+                    reply_text = 'link'
+                elif isinstance(message, LocationMessage):
+                    reply_text = 'location'
+                elif isinstance(message, VideoMessage):
+                    reply_text = 'video'
+                elif isinstance(message, ShortVideoMessage):
+                    reply_text = 'shortvideo'
+                else:
+                    reply_text = 'other'
+                response = wechat_instance.response_text(content=reply_text)
+            except ParseError:    
+                return HttpResponseBadRequest('Invalid XML Data')
+        return HttpResponse(response, content_type="application/xml")
+ 
